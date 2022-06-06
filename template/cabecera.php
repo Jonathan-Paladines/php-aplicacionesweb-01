b39674dc99a5b64b955fa4c2be27588b55a05772
Jonathan-Paladines <84526568+Jonathan-Paladines@users.noreply.github.com>
2022-06-05 21:05:35 -0500

    primer commit, auto preparacion en php

---
+++ template/cabecera.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta chartset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content=IE=edge>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web Administrador de Libros</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+</head>
+<body>
+    <h2>SITIO WEB ADMINISTRADOR DE LIBROS</h2>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Biblioteca</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="libros.php">Libros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">Nosotros</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="container">
+        <br>
+                <div class="row">
